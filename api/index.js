06f1f7fc70c192084c9733792a92c7ce44f2a7e7
han sheng <gh890528@gmail.com>
2023-10-24 15:53:45 +0800

    home page style

--- api/index.js
+++ api/index.js
@@ -33,25 +33,83 @@ app.get('/api/binanceData', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
+//獲取BTC/USDT的最新價格
 async function getAllPrice() {
+  try {
+    const accountInfo = await client.prices();
+    return accountInfo
+  } catch (error) {
+    console.error('獲取賬戶信息時出現錯誤：', error.message);
+  }
+}
+
+app.get('/api/binancePrice', async (req, res) => {
     try {
-      const accountInfo = await client.prices();
-      return accountInfo
+      const ticker = await getLatestPrices(['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'SOLUSDT', 'DOGEUSDT', 'ADAUSDT']);
+      res.json(ticker);
     } catch (error) {
-      console.error('獲取賬戶信息時出現錯誤：', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  async function getLatestPrices(symbols) {
+    try {
+      // 使用 Promise.all 同時發送多個請求
+      const tickers = await Promise.all(
+        symbols.map(async (symbol) => {
+          const ticker = await client.prices({ symbol });
+          return { [symbol]: ticker[symbol] };
+        })
+        );
+        
+        // 將每個交易對的價格整理成一個對象
+        const prices = Object.assign({}, ...tickers);
+      // console.log('最新價格：', prices);
+      return prices
+    } catch (error) {
+      console.error('獲取價格時出現錯誤：', error.message);
     }
   }
-// 獲取BTC/USDT的最新價格
-// async function getLatestPrice() {
-//   try {
-//     const ticker = await client.prices({ symbol: 'ETHUSDT' });
-//     console.log(`BTC/USDT 最新價格：${ticker.ETHUSDT}`);
-//   } catch (error) {
-//     console.error('獲取價格時出現錯誤：', error.message);
-//   }
-// }
 
+  
+  app.get('/api/topGainers', async (req, res) => {
+    try {
+      const stats = await client.dailyStats();
+      const sortedStats = stats.sort((a, b) => parseFloat(b.priceChangePercent) - parseFloat(a.priceChangePercent));
+      const top15Stats = sortedStats.slice(0, 15);
+  
+      const formattedData = top15Stats.map(pair => ({
+        symbol: pair.symbol,
+        price: pair.lastPrice,
+        dailyChange: pair.priceChangePercent,
+      }));
+  
+      res.json(formattedData);
+    } catch (error) {
+      console.error(error.body);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
+  app.get('/api/topLosers', async (req, res) => {
+    try {
+      const stats = await client.dailyStats();
+      const sortedStats = stats.sort((a, b) => parseFloat(a.priceChangePercent) - parseFloat(b.priceChangePercent));
+      const top15Stats = sortedStats.slice(0, 15);
+  
+      const formattedData = top15Stats.map(pair => ({
+        symbol: pair.symbol,
+        price: pair.lastPrice,
+        dailyChange: pair.priceChangePercent,
+      }));
+  
+      res.json(formattedData);
+    } catch (error) {
+      console.error(error.body);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 // // 獲取賬戶信息
 // async function getAccountInfo() {
 //   try {
@@ -66,4 +124,4 @@ async function getAllPrice() {
 // 呼叫函數
 // getLatestPrice();
 // getAccountInfo();
-getAllPrice();
+// getAllPrice();
